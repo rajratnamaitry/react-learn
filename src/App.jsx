dbe6f29fef2bd62c5eddc90931162c7a4dc09c97
rajratnamaitry <rajratnamaitry@gmail.com>
2022-07-03 22:27:25 +0530

    fragment react

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import MessageClass from './components/messageClass'
 import ErrorBindClass from './components/errorBindingClass'
 import MethodAsProps from './components/methodAsProps'
 import LifeCycleA from './components/lifeCycleA'
+import Fragment from './components/fragment'
 function App() {
   return (
     <div className="App">
       {/* <MessageClass name="raj"></MessageClass> */}
       {/* <ErrorBindClass name="error"></ErrorBindClass> */}
       {/* <MethodAsProps></MethodAsProps> */}
-      <LifeCycleA/>
+      {/* <LifeCycleA/> */}
+      <Fragment></Fragment>
     </div>
   )
 }
---
+++ src/components/fragment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Fragment() {
+  return (
+    <React.Fragment>
+        <>
+            <div>Fragment</div>
+        </>
+    </React.Fragment>
+  )
+}
