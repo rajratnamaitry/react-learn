262233d9c262b22d173e6f5e953f414dce3b9545
rajratnamaitry <rajratnamaitry@gmail.com>
2022-08-12 15:21:19 +0530

    doc config.js added

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,17 @@
 module.exports = {
     title: 'React learning',
-    description: 'Just playing around'
+    description: 'Just playing around',
+    themeConfig: {
+        repo: 'rajratnamaitry/react-learn',
+        docsDir: 'docs',
+        smoothScroll: true,
+        displayAllHeaders: true,
+        sidebar: 'auto',
+        searchPlaceholder: 'Search...',
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Basics', link: '/topics/basics.html' },
+          { text: 'Hooks', link: '/topics/hooks.html' }
+        ]
+      }
 }
